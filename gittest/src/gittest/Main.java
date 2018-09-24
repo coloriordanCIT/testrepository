f4df0e07789aaa19591653848ccf24daa121f36c
colinoriordan <colinoriordan@192.168.1.9>
2018-09-24 19:18:33 +0100

    This is my second commit from my mac desktop

--- gittest/src/gittest/Main.java
+++ gittest/src/gittest/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Testing out git repositories!");
+		System.out.println("This line was added from my mac desktop!");
 
 	}
 
